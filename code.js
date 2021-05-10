5484de5a131d53e50be31976f6c8822aff228c15
warriorcoder310 <69365459+warriorcoder310@users.noreply.github.com>
2021-05-10 21:44:50 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,113 @@
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen1");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button4", "click", function( ) {
+  setScreen("screen4");
+  setText("text_area9", "Thank you  " + getText("text_input1"));
+});
+onEvent("button5", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("radio_button1", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("radio_button6", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("radio_button10", "click", function( ) {
+  setScreen("screen8");
+});
+onEvent("radio_button3", "click", function( ) {
+  setScreen("screen9");
+});
+onEvent("button7", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button8", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("button10", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button9", "click", function( ) {
+  setScreen("screen11");
+});
+onEvent("radio_button4", "click", function( ) {
+  setScreen("screen13");
+});
+onEvent("button12", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button14", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("button14", "click", function( ) {
+  setScreen("screen11");
+});
+onEvent("radio_button5", "click", function( ) {
+  setScreen("screen14");
+});
+onEvent("button15", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button16", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("button17", "click", function( ) {
+  setScreen("screen11");
+});
+onEvent("button13", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("button11", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button18", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("button6", "click", function( ) {
+  setScreen("screen3");
+});
+console.log(getUserId());
+onEvent("radio_button7", "click", function( ) {
+  setScreen("screen15");
+});
+onEvent("button19", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button20", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("button21", "click", function( ) {
+  setScreen("screen11");
+});
+onEvent("radio_button8", "click", function( ) {
+  setScreen("screen16");
+});
+onEvent("button22", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button23", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("button24", "click", function( ) {
+  setScreen("screen11");
+});
+onEvent("radio_button9", "click", function( ) {
+  setScreen("screen17");
+});
+onEvent("button25", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button26", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("button27", "click", function( ) {
+  setScreen("screen11");
+});
